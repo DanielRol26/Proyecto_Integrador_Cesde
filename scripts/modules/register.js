d6b5c2345222116eb3d78ce4681223ab0fd3ec8a
AndresAcosta01 <andresacost12a@gmail.com>
2026-05-07 01:09:57 -0500

    refactor: Se actualiza lógica de js

--- scripts/modules/register.js
+++ scripts/modules/register.js
@@ -1,10 +1,3 @@
-// const dateInput = document.getElementById('birth-date');
-// const calendarIcon = document.getElementById('calendarIcon');
-
-// calendarIcon.addEventListener("click", () => {
-//     dateInput.showPicker();
-// });
-
 const inputFullName = document.getElementById('fullname');
 const selectDocumentType = document.getElementById('document-type');
 const inputDocumentNumber = document.getElementById('document-number');
@@ -57,10 +50,11 @@ formulary.addEventListener('submit', (e) => {
         documentNumberError.classList.add('error');
         return
     }
+    let gender = '';
     if (radioMale.checked) {
-        const gender = radioMale.value;
+        gender = radioMale.value;
     } else if (radioFemale.checked) {
-        const gender = radioFemale.value;
+        gender = radioFemale.value;
     } else {
         genderError.textContent = 'Seleccione una de las opciones';
         genderError.classList.add('error');
@@ -81,6 +75,7 @@ formulary.addEventListener('submit', (e) => {
         emailError.classList.add('error');
         return
     }
+    let password = '';
     if (inputPassword.value.trim() === '') {
         passwordError.textContent = 'La contraseña no puede estar vacía';
         passwordError.classList.add('error');
@@ -88,21 +83,24 @@ formulary.addEventListener('submit', (e) => {
         confirmPasswordError.textContent = 'Debe confirmar su contraseña';
         confirmPasswordError.classList.add('error');
     } else if (inputPassword.value === inputConfirmPassword.value) {
-        const password = inputPassword.value;
+        password = inputPassword.value;
     } else {
         passwordError.textContent = 'Las contraseñas no coinciden';
         passwordError.classList.add('error');
+        return
     }
     if (!checkboxTermsPrivacy.checked) {
         termsPrivacyError.textContent = 'Para continuar con la creación de la cuenta debes aceptar los términos y condiciones'
         termsPrivacyError.classList.add('error');
+        return
     }
 
-    const newRegisteredUser = { fullName, documentType, documentNumber, gender, birthDate, email, password };
+    const newRegisteredUser = { fullName, documentType, documentNumber, gender, birthDate, email, password, termsPrivacy, emailPromotions };
 
     registeredUsers = [...registeredUsers, newRegisteredUser];
 
     formulary.reset();
+    showUsers(registeredUsers);
 })
 
 function clearError(input, errorSpan) {
@@ -193,12 +191,103 @@ filterType.addEventListener('change', () => {
     }
 });
 
-const filterType = document.getElementById('filter-type');
-const filterValue = document.getElementById('filter-value');
 const btnFilter = document.getElementById('btn-filter');
 const btnShowAll = document.getElementById('btn-show-all');
 const usersList = document.getElementById('users-list');
 
-btnShowAll.addEventListener('click', () =>{
-    
+function showUsers(users) {
+
+    usersList.innerHTML = '';
+
+    users.forEach(user => {
+
+        const card = document.createElement('li');
+
+        card.classList.add(
+            'flex',
+            'flex-col',
+            'gap-[12px]',
+            'bg-[#fcfdff]',
+            'border',
+            'border-[#dbe4f0]',
+            'rounded-[16px]',
+            'p-[22px]',
+            'shadow-[0_2px_10px_rgba(15,23,42,0.05)]',
+            'w-full',
+            'max-w-[390px]',
+            'min-h-[360px]',
+            'list-none'
+        );
+
+        const name = document.createElement('p');
+        name.textContent = `Nombre Uusario: ${user.fullName}`;
+
+        const documentType = document.createElement('p');
+        documentType.textContent = `Tipo de documento: ${user.documentType}`;
+
+        const documentNumber = document.createElement('p');
+        documentNumber.textContent = `Número de documento: ${user.documentNumber}`;
+
+        const gender = document.createElement('p');
+        gender.textContent = `Sexo: ${user.gender}`;
+
+        const birthDate = document.createElement('p');
+        birthDate.textContent = `fecha de nacimiento: ${user.birthDate}`;
+
+        const email = document.createElement('p');
+        email.textContent = `Correo electrónico: ${user.email}`;
+
+        const password = document.createElement('p');
+        password.textContent = `Contraseña: ${user.password}`;
+
+        const termsPrivacy = document.createElement('p');
+        termsPrivacy.textContent = `¿Aceptó terminos y condiciones?: ${user.termsPrivacy}`;
+
+        const emailPromotions = document.createElement('p');
+        emailPromotions.textContent = `¿Aceptó recibir información de promociones y descuentos?: ${user.emailPromotions}`;
+
+
+        card.appendChild(name);
+        card.appendChild(documentType);
+        card.appendChild(documentNumber);
+        card.appendChild(gender);
+        card.appendChild(birthDate);
+        card.appendChild(email);
+        card.appendChild(password);
+        card.appendChild(termsPrivacy);
+        card.appendChild(emailPromotions);
+        usersList.appendChild(card);
+    });
+}
+
+btnShowAll.addEventListener('click', () => {
+    showUsers(registeredUsers);
 })
+
+btnFilter.addEventListener('click', () => {
+
+    const type = filterType.value;
+    const value = filterValue.value;
+
+    let filteredUsers = [];
+
+    registeredUsers.forEach(user => {
+
+        if (type === 'document-type' && user.documentType === value) {
+            filteredUsers.push(user);
+        } else if (type === 'gender' && user.gender === value) {
+            filteredUsers.push(user);
+        } else if (type === 'email' && user.email.includes(value)) {
+            filteredUsers.push(user);
+        } else if (type === 'name' && user.fullName.charAt(0).toUpperCase() === value) {
+            filteredUsers.push(user);
+        }
+    })
+    showUsers(filteredUsers);
+});
+
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('flex');
+}
